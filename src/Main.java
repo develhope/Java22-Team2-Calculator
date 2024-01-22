177b52c57f20c167af6618476cfe3ca488a2d841
Michele <michele.saporito98@yahoo.com>
2024-01-22 16:14:50 +0100

    inseriti calcoli aritmetici e calcolo esponenziale

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,9 @@ import java.sql.SQLOutput;
 public class Main {
 
     public static void main(String[] args) {
-
+        System.out.println(calcolaPotenza(2, ));
 
     }
-
     public static void pariDispari(int a) {
         if (a % 2 == 0) {
             System.out.println("Numero pari");
@@ -15,4 +14,33 @@ public class Main {
         }
     }
 
+    public static int addizione(int a, int b) {
+        return a + b;
+    }
+
+    public static int sottrazione(int a, int b) {
+        return a - b;
+    }
+
+    public static int moltiplicazione(int a, int b) {
+        return a * b;
+    }
+
+    public static int divisione(int a, int b) {
+        return a / b;
+    }
+
+    public static int calcolaPotenza(int base, int exp) {
+        int result = 1;
+        if(base > 0 && exp == 0) {
+            return result;
+        } else if (base == 0 && exp >= 1) {
+            return 0;
+        } else {
+            for(int i = 1; i <= exp; i++) {
+                result = result * base;
+            }
+            return result;
+        }
+    }
 }
